ea4fab110bb283c760666cbb78352527984ce573
Chaitanya <yourchaitu@gmail.com>
2021-02-25 19:35:42 +0530

    Initial Commit

---
+++ source/components/Container.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Twitter from '../components/Twitter';
+import IdeaMarket from '../components/IdeaMarket';
+
+const Options: React.FC = () => {
+  return (
+    <>
+      <Twitter>
+        <IdeaMarket />
+      </Twitter>
+    </>
+  );
+};
+
+
+
+export default Options;
---
+++ source/components/IdeaMarket.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const IdeaMarket: React.FC = () => {
+  return (
+    <>
+      <div style={{color: 'white'}}>IdeaMarket</div>
+    </>
+  );
+};
+
+export default IdeaMarket;
