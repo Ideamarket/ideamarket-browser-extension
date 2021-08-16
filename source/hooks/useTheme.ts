6cce63854fb668fe49dca0a16d1e8f6edcf633a6
Shmoji <jackj_skate@hotmail.com>
2021-08-16 08:55:55 -0400

    fix: dynamically update theme after changing extension options

--- source/hooks/useTheme.ts
+++ source/hooks/useTheme.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
-import { getTheme, intialSelectedTheme } from "../helpers/theme"
+import { browser } from "webextension-polyfill-ts"
+import { startIdeaMarket } from "../contentScript/ideaMarket"
+import { getTheme, intialSelectedTheme, setThemeWithSelection, ThemeType } from "../helpers/theme"
 
 export default function useTheme() {
   const [theme, setTheme] = useState(intialSelectedTheme)
@@ -7,16 +9,26 @@ export default function useTheme() {
   useEffect(() => {
     // This listener updates theme dynamically based on System Default
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', initTheme)
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/onChanged
+    browser.storage.onChanged.addListener(optionsChanged)
 
     async function initTheme() {
       const themeResponse = await getTheme()
       setTheme(themeResponse)
     }
 
+    function optionsChanged(changes?: any, areaName?: string) {
+      const { newValue = ThemeType.Light } = changes?.theme
+      setTheme(newValue)
+      startIdeaMarket() // Need to rerender after setting new theme
+      setThemeWithSelection()
+    }
+
     initTheme()
 
     return () => {
       window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', initTheme)
+      browser.storage.onChanged.removeListener(optionsChanged)
     }
   }, [])
 
